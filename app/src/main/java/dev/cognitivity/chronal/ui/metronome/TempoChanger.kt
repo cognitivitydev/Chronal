58ec64f5f3685ce8df1abf3e131edb3255ba45f4
cognitivity <cognitivitydev@gmail.com>
2025-11-15 21:03:21 -0500

    Increase size of tempo buttons

--- app/src/main/java/dev/cognitivity/chronal/ui/metronome/TempoChanger.kt
+++ app/src/main/java/dev/cognitivity/chronal/ui/metronome/TempoChanger.kt
@@ -18,12 +18,14 @@
 
 package dev.cognitivity.chronal.ui.metronome
 
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
@@ -31,10 +33,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.input.pointer.changedToDown
+import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -43,70 +54,54 @@ import dev.cognitivity.chronal.ChronalApp.Companion.context
 import dev.cognitivity.chronal.R
 import dev.cognitivity.chronal.ui.metronome.windows.setBPM
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 @Composable
 fun BoxScope.TempoChanger() {
     val metronome = ChronalApp.getInstance().metronome
+    val track = metronome.getTrack(0)
+    val scope = rememberCoroutineScope()
+
+    var iconsBounds by remember { mutableStateOf<Rect?>(null) }
+    var overlayOffset by remember { mutableStateOf(Offset.Zero) }
+
     Column(
         modifier = Modifier.align(Alignment.Center)
     ) {
         Row {
-            val scope = rememberCoroutineScope()
-
             Column(
-                modifier = Modifier.align(Alignment.CenterVertically)
-                    .padding(2.dp, 0.dp, 2.dp, 0.dp)
+                modifier = Modifier
+                    .onGloballyPositioned { coords ->
+                        val pos = coords.localToRoot(Offset.Zero)
+                        val size = coords.size
+                        iconsBounds = Rect(
+                            pos.x,
+                            pos.y,
+                            pos.x + size.width.toFloat(),
+                            pos.y + size.height.toFloat()
+                        )
+                    }
+                    .align(Alignment.CenterVertically)
+                    .padding(horizontal = 0.dp)
             ) {
                 Icon(
                     imageVector = Icons.Outlined.KeyboardArrowUp,
                     contentDescription = context.getString(R.string.metronome_increase_tempo),
-                    modifier = Modifier.pointerInput(Unit) {
-                            detectTapGestures(
-                                onPress = {
-                                    var isHeld = true
-                                    scope.launch {
-                                        setBPM(metronome.getTrack(0).bpm + 1)
-                                        delay(500)
-                                        while (isHeld) {
-                                            setBPM(metronome.getTrack(0).bpm + 1)
-                                            delay(50)
-                                        }
-                                    }
-                                    tryAwaitRelease()
-                                    isHeld = false
-                                }
-                            )
-                        },
+                    modifier = Modifier.size(28.dp),
                     tint = MaterialTheme.colorScheme.tertiary
                 )
                 Icon(
                     imageVector = Icons.Outlined.KeyboardArrowDown,
                     contentDescription = "Decrease tempo",
-                    modifier = Modifier.pointerInput(Unit) {
-                            detectTapGestures(
-                                onPress = {
-                                    var isHeld = true
-                                    scope.launch {
-                                        setBPM(metronome.getTrack(0).bpm - 1)
-
-                                        delay(500)
-                                        while (isHeld) {
-                                            setBPM(metronome.getTrack(0).bpm - 1)
-                                            delay(50)
-                                        }
-                                    }
-                                    tryAwaitRelease()
-                                    isHeld = false
-                                }
-                            )
-                        },
+                    modifier = Modifier.size(28.dp),
                     tint = MaterialTheme.colorScheme.tertiary
                 )
             }
+
             Text(
                 modifier = Modifier.align(Alignment.CenterVertically),
-                text = metronome.getTrack(0).bpm.toInt().toString(),
+                text = track.bpm.toInt().toString(),
                 style = MaterialTheme.typography.displayLarge,
                 color = MaterialTheme.colorScheme.onSurface,
                 maxLines = 1,
@@ -124,7 +119,7 @@ fun BoxScope.TempoChanger() {
         }
 
         val markings = ChronalApp.getInstance().settings.tempoMarkings.value.reversed()
-        val marking = markings.firstOrNull { it.range.contains(metronome.getTrack(0).bpm.toInt()) }
+        val marking = markings.firstOrNull { it.range.contains(track.bpm.toInt()) }
         val string = marking?.name ?: context.getString(R.string.metronome_tempo_unknown)
 
         Text(
@@ -136,4 +131,65 @@ fun BoxScope.TempoChanger() {
                 .offset(y = (-8).dp)
         )
     }
+
+    // expanded bounds for arrows
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .onGloballyPositioned { coords -> overlayOffset = coords.localToRoot(Offset.Zero) }
+            .pointerInput(Unit) {
+                val paddingX = 24.dp.toPx()
+                val paddingY = 12.dp.toPx()
+
+                while (true) {
+                    awaitPointerEventScope {
+                        val event = awaitPointerEvent()
+                        val down = event.changes.firstOrNull { it.changedToDown() }
+                        if (down == null) return@awaitPointerEventScope
+
+                        val bounds = iconsBounds
+                        if (bounds == null) {
+                            down.consume()
+                            return@awaitPointerEventScope
+                        }
+
+                        val expanded = Rect(
+                            bounds.left - paddingX,
+                            bounds.top - paddingY,
+                            bounds.right + paddingX,
+                            bounds.bottom + paddingY
+                        )
+
+                        var currentPos = overlayOffset + down.position
+                        if(currentPos !in expanded) return@awaitPointerEventScope
+                        down.consume()
+
+                        val job = scope.launch {
+                            var isUp = currentPos.y < bounds.center.y
+                            setBPM(if (isUp) track.bpm + 1 else track.bpm - 1)
+                            delay(500)
+                            while (isActive) {
+                                if (currentPos !in expanded) break
+
+                                isUp = currentPos.y < bounds.center.y
+                                setBPM(if (isUp) track.bpm + 1 else track.bpm - 1)
+                                delay(50)
+                            }
+                        }
+
+                        while (true) {
+                            val moveEvent = awaitPointerEvent()
+                            val change = moveEvent.changes.firstOrNull() ?: continue
+                            currentPos = overlayOffset + change.position
+                            val up = moveEvent.changes.firstOrNull { it.changedToUp() }
+                            if (up != null) {
+                                up.consume()
+                                break
+                            }
+                        }
+                        job.cancel()
+                    }
+                }
+            }
+    )
 }
